1ab9c0829662f774bfd626dfea0b59ac22d57535
mcollins <fixitman.mike@gmail.com>
2022-07-19 00:45:36 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const Mongoose = require('mongoose')
+
+const userSchema = new Mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    hashedPW: {
+        type: String,
+        required: true,
+    },
+    
+})
+
+userSchema.virtual('name').get(function(){
+    return `${this.firstName} ${this.lastName}`
+})
+
+module.exports = Mongoose.model('User', userSchema)
---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const path = require('path')
+const createError = require('http-errors');
+const logger = require('morgan');
+//const rootRoutes = require('./routes/rootRoutes');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose')
+const app = express();
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
+require('dotenv').config('./.env');
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log('Could not connect to DB', err)
+        process.exit(1);
+    })
+
+
+/**
+ * Session set up
+ */
+
+const store = MongoStore.create({
+    mongoUrl: process.env.MONGO_URI,
+    collection: 'session'
+})
+store.on('error', function (error) {
+    console.log(error);
+})
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    saveUninitialized: false,
+    resave: false,
+    store: store
+}));
+
+
+/**
+ * App set up
+ */
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('tiny'));
+
+
+/**
+ * ROUTES
+ */
+
+//app.use('/', rootRoutes)
+app.use('/auth', authRoutes)
+
+
+/**
+ * Errors
+ */
+
+app.use((req, res, next) => {
+    return next(createError(404, 'page not found'))
+})
+
+app.use((err, req, res, next) => {
+    if (err) {
+        console.log(err.message);
+        res.send(err.message)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
+
+
+
