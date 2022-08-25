90dbfa84d02081bcc76aa01e8b3df55173503f6b
mcollins <fixitman.mike@gmail.com>
2022-08-25 01:14:26 -0400

    8-24-22

--- frontend/src/theme/colors.js
+++ frontend/src/theme/colors.js
@@ -3,8 +3,6 @@
 
 const coolorsColors = JSON.parse('{"Phthalo Green":"23432e","Amazon":"3f7853","GO Green":"46a667","Honeydew":"d4eddd","Misty Rose":"f5e3df","Isabelline":"f8f5f1","Linen":"faefe1","Bone":"ebe2d6","Camel":"b18d59","Cafe Noir":"564424"}')
 
-
-
 const keys = Object.keys(coolorsColors)
 const colors = {}
 
@@ -12,9 +10,5 @@ keys.forEach((key) => {
     colors[key] = `#${coolorsColors[key]}`
 })
 
-
-
 export default colors
-
-
 
