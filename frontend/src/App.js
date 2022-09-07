40f548705a9d6dc93862e43cf12bfe866be8e8e5
mcollins <fixitman.mike@gmail.com>
2022-09-07 02:17:40 -0400

    Updated routing setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Error404 from './pages/Error404'
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import RequireAuth from './components/RequireAuth';
 import SecretPage from './pages/SecretPage';
-import ErrorDisplay from './components/ErrorDisplay';
 import { ThemeProvider } from '@mui/material'
 import appTheme from './theme/theme';
 import Layout from './pages/layout';
@@ -24,24 +23,19 @@ function App() {
 
     return (
       <>
-        <BrowserRouter>
-          <ThemeProvider theme={appTheme}>
-            <ErrorDisplay />
-
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path='/' element={<RequireAuth />}>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/secrets' element={< SecretPage />} />
-                </Route>
-                <Route path='login' element={<LoginPage />} />
-                <Route path='register' element={<RegisterPage />} />
-                <Route path='*' element={<Error404 />} />
+        <ThemeProvider theme={appTheme}>          
+          <Routes>
+            <Route path='/' element={<Layout />}>
+              <Route path='login' element={<LoginPage />} />
+              <Route path='register' element={<RegisterPage />} />
+              <Route path='/' element={<RequireAuth />}>
+                <Route index element={<Home />} />
+                <Route path='secrets' element={< SecretPage />} />
               </Route>
-            </Routes>
-
-          </ThemeProvider>
-        </BrowserRouter>
+              <Route path='*' element={<Error404 />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
       </>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import { StoreProvider } from 'easy-peasy'
 import store from './store'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
     <StoreProvider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App/>} />
+        </Routes>
+      </BrowserRouter>      
     </StoreProvider>
   
 );
